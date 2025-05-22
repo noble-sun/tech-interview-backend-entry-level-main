843e24be70d8062f291de9c651bfd568e55586d8
noble-sun <matsubayashi.helio+github@gmail.com>
2025-05-22 10:25:53 -0300

    test: request and service tests

--- spec/requests/carts_spec.rb
+++ spec/requests/carts_spec.rb
@@ -3,9 +3,58 @@ require 'rails_helper'
 RSpec.describe "/carts", type: :request do
   pending "TODO: Escreva os testes de comportamento do controller de carrinho necessários para cobrir a sua implmentação #{__FILE__}"
   describe "POST /add_items" do
-    let(:cart) { Cart.create }
-    let(:product) { Product.create(name: "Test Product", price: 10.0) }
-    let!(:cart_item) { CartItem.create(cart: cart, product: product, quantity: 1) }
+    context 'add a new product to cart' do
+      context 'when cart do not exist' do
+        it 'creates a new cart and and save id in session' do
+          product = create(:product)
+
+          post carts_add_items_path, params: { product_id: product.id, quantity: 2 }
+
+          expect(response).to have_http_status(:success)
+
+          parsed_response = JSON.parse(response.body, symbolize_names: true)
+          expect(parsed_response).to include(
+             :id, :total_price, :products => a_collection_containing_exactly(
+              hash_including(:id, :name, :quantity, :unit_price, :total_price)
+            )
+          )
+          expect(request.session[:cart_id]).to eq(Cart.last.id)
+        end
+      end
+
+      context 'when cart already exist' do
+        it 'update the current cart and add the product' do
+          cart = create(:cart)
+          product = create(:product)
+
+          allow_any_instance_of(ActionDispatch::Request::Session)
+            .to receive(:[]).with(:cart_id).and_return(cart.id)
+
+          post carts_add_items_path, params: { product_id: product.id, quantity: 2 }
+
+          expect(response).to have_http_status(:success)
+          expect(Cart.count).to eq(1)
+          expect(request.session[:cart_id]).to eq(cart.id)
+
+          parsed_response = JSON.parse(response.body, symbolize_names: true)
+          expect(parsed_response).to include(
+             :id, :total_price, :products => a_collection_containing_exactly(
+              hash_including(:id, :name, :quantity, :unit_price, :total_price)
+            )
+          )
+        end
+      end
+
+      context 'when product does not exist' do
+        it 'return error message' do
+          post carts_add_items_path, params: { product_id: 123, quantity: 2 }
+
+          expect(response).to have_http_status(:not_found)
+          expect(JSON.parse(response.body, symbolize_names: true))
+            .to eq({error: "Product does not exist."})
+        end
+      end
+    end
 
     context 'when the product already is in the cart' do
       subject do
@@ -14,6 +63,10 @@ RSpec.describe "/carts", type: :request do
       end
 
       it 'updates the quantity of the existing item in the cart' do
+        cart = create(:cart)
+        product = create(:product, name: "Test Product", price: 10.0)
+        cart_item = create(:cart_item, cart:, product:)
+
         expect { subject }.to change { cart_item.reload.quantity }.by(2)
       end
     end
