abed2e3ef637807762545f44daed71f5c8476629
noble-sun <matsubayashi.helio+github@gmail.com>
2025-05-22 10:25:16 -0300

    feat: controller endpoint to add product to cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,25 @@
 class CartsController < ApplicationController
   ## TODO Escreva a lógica dos carrinhos aqui
+
+  def add_items
+    cart = Cart.find_or_create_by!(id: session[:cart_id])
+
+    result = AddProductToCartService.call(
+      cart:,
+      product_id: cart_params[:product_id],
+      quantity: cart_params[:quantity]
+    )
+
+    session[:cart_id] = cart.id if result
+
+    render json: cart, serializer: CartSerializer, status: :ok
+  rescue ProductNotFoundError
+    render json: { error: "Product does not exist." }, status: :not_found
+  end
+
+  private
+
+  def cart_params
+    params.permit(:product_id, :quantity)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq'
   resources :products
-  get "up" => "rails/health#show", as: :rails_health_check
 
+  post "carts/add_items", to: "carts#add_items"
+
+  get "up" => "rails/health#show", as: :rails_health_check
   root "rails/health#show"
 end
