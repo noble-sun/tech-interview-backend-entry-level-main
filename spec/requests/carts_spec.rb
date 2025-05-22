f8a104d812cf05b1cd7f5d9f535cfae957e8c430
noble-sun <matsubayashi.helio+github@gmail.com>
2025-05-22 16:47:11 -0300

    test: add tests for add_item endpoint

--- spec/requests/carts_spec.rb
+++ spec/requests/carts_spec.rb
@@ -49,7 +49,7 @@ RSpec.describe "Carts", type: :request do
 
           expect(response).to have_http_status(:not_found)
           expect(response.parsed_body.deep_symbolize_keys)
-            .to eq({error: "Product does not exist."})
+            .to eq({error: "Product not found."})
         end
       end
     end
@@ -106,20 +106,49 @@ RSpec.describe "Carts", type: :request do
     end
   end
 
-  describe "POST /add_items" do
+  describe "POST /cart/add_item" do
     context 'when the product already is in the cart' do
       let(:product) { create(:product, name: "Test Product", price: 10.0) }
+      let(:cart) { create(:cart) }
+      let!(:cart_item) { create(:cart_item, cart:, product:) }
 
       subject do
-        post '/cart/add_items', params: { product_id: product.id, quantity: 1 }, as: :json
-        post '/cart/add_items', params: { product_id: product.id, quantity: 1 }, as: :json
+        post '/cart/add_item', params: { product_id: product.id, quantity: 1 }, as: :json
+        post '/cart/add_item', params: { product_id: product.id, quantity: 1 }, as: :json
       end
 
       it 'updates the quantity of the existing item in the cart' do
-        cart = create(:cart)
-        cart_item = create(:cart_item, cart:, product:)
+        allow_any_instance_of(ActionDispatch::Request::Session)
+          .to receive(:[]).with(:cart_id).and_return(cart.id)
 
         expect { subject }.to change { cart_item.reload.quantity }.by(2)
+      end
+    end
+
+    context "when product does not exist" do
+      it 'return error message' do
+        cart = create(:cart)
+
+        allow_any_instance_of(ActionDispatch::Request::Session)
+          .to receive(:[]).with(:cart_id).and_return(cart.id)
+
+        post add_item_cart_path, params: { product_id: 123, quantity: 2 }
+
+        expect(response).to have_http_status(:not_found)
+        expect(response.parsed_body.deep_symbolize_keys)
+          .to eq({error: "Product not found."})
+      end
+    end
+
+    context "when there isn't a cart available" do
+      it "return error" do
+        product = create(:product)
+
+        post add_item_cart_path, params: { product_id: product.id, quantity: 2 }
+
+        expect(response).to have_http_status(:not_found)
+        expect(response.parsed_body.deep_symbolize_keys)
+          .to eq({error: "Cart not found."})
       end
     end
   end
---
+++ spec/services/add_or_update_cart_item_service_spec.rb
@@ -0,0 +1,76 @@
+require "rails_helper"
+
+RSpec.describe AddOrUpdateCartItemService, type: :service do
+  describe ".call" do
+    context "add a new product to cart" do
+      it "successfully" do
+        product = create(:product, price: 10.0)
+        cart = create(:cart)
+
+        result = described_class.call(cart:, product_id: product.id, quantity: 2)
+
+        cart.reload
+        expect(result).to be_truthy
+        expect(cart.cart_items.count).to eq(1)
+        expect(cart.total_price).to eq(20.0)
+
+        item = cart.cart_items.last
+        expect(item.product).to eq(product)
+        expect(item.quantity).to eq(2)
+        expect(item.unit_price).to eq(product.price)
+        expect(item.total_price).to eq(20.0)
+      end
+
+      context "when the is a different product on the cart" do
+        it "create a new cart_item for the new product" do
+          product = create(:product, price: 10.0)
+          cart = create(:cart)
+          create(:cart_item, cart:, product:, quantity: 2, unit_price: product.price, total_price: 20.0)
+          product_not_on_cart = create(:product, price: 15.0)
+
+          result = described_class.call(cart:, product_id: product_not_on_cart.id, quantity: 2)
+
+          cart.reload
+          expect(result).to be_truthy
+          expect(cart.cart_items.count).to eq(2)
+          expect(cart.total_price).to eq(50.0)
+
+          item = cart.cart_items.last
+          expect(item.product).to eq(product_not_on_cart)
+          expect(item.quantity).to eq(2)
+          expect(item.unit_price).to eq(product_not_on_cart.price)
+          expect(item.total_price).to eq(30.0)
+        end
+      end
+    end
+
+    context "when product does not exist" do
+      it "raises error" do
+        cart = create(:cart)
+
+        expect { described_class.call(cart:, product_id: 123, quantity: 2) }
+          .to raise_error(ProductNotFoundError)
+
+        cart.reload
+        expect(cart.cart_items.count).to eq(0)
+      end
+    end
+
+    context "when there is already the same product on the cart" do
+      it "update and sum the current quantity on the cart_item" do
+        cart = create(:cart, total_price: 20.0)
+        product = create(:product, price: 10.0)
+        create(:cart_item, cart:, product:, quantity: 2, unit_price: 10.0, total_price: 20.0) 
+
+        result = described_class.call(cart:, product_id: product.id, quantity: 1)
+
+        cart.reload
+        expect(result).to be_truthy
+        expect(cart.cart_items.count).to eq(1)
+        expect(cart.total_price).to eq(30.0)
+        expect(cart.cart_items.last.quantity).to eq(3)
+        expect(cart.cart_items.last.total_price).to eq(30.0)
+      end
+    end
+  end
+end
