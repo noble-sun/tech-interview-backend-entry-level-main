af26812b0d9b9ea9c9131070072a00419fedf526
noble-sun <matsubayashi.helio+github@gmail.com>
2025-05-23 02:40:10 -0300

    chore: configure sidekiq-scheduler

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,15 @@
+require 'sidekiq'
+require 'sidekiq-scheduler'
+
+Sidekiq.configure_server do |config|
+  config.on(:startup) do
+    Sidekiq.schedule = YAML.load_file(
+      File.expand_path(
+        '../sidekiq_scheduler.yml',
+        File.dirname(__FILE__)
+      )
+    )
+
+    SidekiqScheduler::Scheduler.instance.reload_schedule!
+  end
+end
